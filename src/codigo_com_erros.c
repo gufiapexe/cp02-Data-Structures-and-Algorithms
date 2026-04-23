7d082d1b32fbbb9c4bb7d6eecec570ca2b589d07
gufiapexe <guhkunitaki@gmail.com>
2026-04-23 08:36:17 -0300

    Correção do código (comentado)

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,17 +1,17 @@
-#include "stdio.h"
+#include "stdio.h" //falta o "<>"
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media //falta o ";"
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", nota2); //falta o "& no nota2"
 
     media = nota1 + nota2 / 2;
 
-    if (media = 6) {
+    if (media = 6) { //Tem um erro de lógica, pois o aluno nesse caso só seria aprovado com a nota 6, o certo seria >= 6.
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
