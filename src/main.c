9e63960eb53a61239a593585097b9857ade1f45a
gufiapexe <guhkunitaki@gmail.com>
2026-04-23 08:57:40 -0300

    Variável "opcao" declarada, implementaçao do switch case

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>  
 
 int main() {
+    int opcao;
     float nota1, nota2, media;
+    printf("Digite a opcao: ");
+    scanf("%d", &opcao);
+    switch(opcao){
+        case 1: 
 
+    }
 
     
     return 0;
