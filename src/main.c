b691c40e11f11c6e876638c89b3fdfb6f3e2dcfe
victorcbinot <victorcbinot@gmail.com>
2026-04-23 09:10:44 -0300

    Função média aritimética

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>  
 
+float media_aritimetica (float nota1, float nota2){
+    return (nota1 + nota2) / 2;
+}
 int main() {
     int opcao;
-    float nota1, nota2, media;
+    float n1, n2, media;
+
     printf("1 - Cálculo de média\n");
     printf("2 - Classificação do aluno\n");
     printf("Digite a opcao: ");
     scanf("%d", &opcao);
     switch(opcao){
-        case 1: 
+        case 1:
 
     }
 
